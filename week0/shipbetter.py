feb31a827032bae1f168e533befe2598df72184b
alice <89223922+tangalice@users.noreply.github.com>
2022-03-30 19:43:35 +0000

    changed ship animation to have different code from original

--- week0/shipbetter.py
+++ week0/shipbetter.py
@@ -5,7 +5,7 @@ import time
 ANSI_CLEAR_SCREEN = u"\u001B[2J"
 ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
 OCEAN_COLOR = u"\u001B[44m\u001B[2D"
-SHIP_COLOR = u"\u001B[32m\u001B[2D"
+SHIP_COLOR = u"\u001b[31m"
 RESET_COLOR = u"\u001B[0m\u001B[2D"
 
 def ocean_print():
